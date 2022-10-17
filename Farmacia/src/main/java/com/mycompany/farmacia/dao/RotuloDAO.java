9d67590a735069e9cad95e0f9eebf12c2057b2a0
Aluno <Aluno@DECOM-LAB-024>
2022-10-17 09:40:24 -0300

    Implementação da classe RotuloDAO #RF003 #3

--- Farmacia/src/main/java/com/mycompany/farmacia/dao/RotuloDAO.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/dao/RotuloDAO.java
@@ -1,5 +1,51 @@
 package com.mycompany.farmacia.dao;
 
+import com.mycompany.farmacia.common.PersistenciaException;
+import com.mycompany.farmacia.dto.Rotulo;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RotuloDAO {
-    
+   
+    public static List<Rotulo> rotulos;
+    private static int chave;
+   
+    static {
+        rotulos = new ArrayList<>();
+        chave = 1;
+    }
+   
+    private RotuloDAO(){}
+   
+    public static Rotulo consultarPorNome (String nome){
+       
+        for (Rotulo r: rotulos){
+            if(r.getNome().equals(nome)){
+                return r;
+            }
+        }
+       
+        return null;
+    }
+   
+    public static Rotulo cadastrar (String nome) throws PersistenciaException{
+       
+        if (consultarPorNome(nome) == null){
+            Rotulo r = new Rotulo(chave, nome);
+            rotulos.add(r);
+            chave++;
+            return r;
+        }
+     
+        throw new PersistenciaException("Violação de campo único: nome");
+    }
+   
+    public static List<Rotulo> rotulos(){
+        if(rotulos.isEmpty()!= true){
+            return rotulos;
+        }
+       
+        return null;
+    }
+   
 }
