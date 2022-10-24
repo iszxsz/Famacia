b4eff4950e81d87956c1ffcb1a856b18939d4a27
Aluno <Aluno@DECOM-LAB-074>
2022-10-24 08:08:44 -0300

    Criação e implementação do BD desktop #RF006 #6

--- Farmacia/src/main/java/com/mycompany/farmacia/Farmacia.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/Farmacia.java
@@ -1,16 +1,13 @@
 package com.mycompany.farmacia;
 
-import com.mycompany.farmacia.dao.ProdutoDAO;
-import com.mycompany.farmacia.dto.Rotulo;
+import com.mycompany.farmacia.view.TelaInicial;
 
 public class Farmacia {
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                
-                ProdutoDAO.cadastrarProduto(new Rotulo(1, "Teste"), 2, 1.00, false, "Dorflex", "10/10/2023");
-                //new TelaInicial().setVisible(true);
+                new TelaInicial().setVisible(true);
             }
         });
     }
--- Farmacia/src/main/java/com/mycompany/farmacia/bancoDeDados/CaixaBD.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/bancoDeDados/CaixaBD.java
@@ -5,10 +5,34 @@
  */
 package com.mycompany.farmacia.bancoDeDados;
 
-/**
- *
- * @author Aluno
- */
+import java.sql.*;
+
 public class CaixaBD {
-    //CAIXA
+
+    public static Connection conectar() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/farmaciagrupo4", "drogaspoint", "cefet123");
+            System.out.println("Conectou no banco de dados.");
+        } catch (SQLException ex) {
+            System.out.println("Erro: Não conseguiu conectar no BD.");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erro: Não encontrou o driver do BD.");
+        }
+
+        return conn;
+    }
+
+    public static void desconectar(Connection conn) {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Desconectou do banco de dados.");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu desconectar do BD.");
+        }
+    }
+
 }
