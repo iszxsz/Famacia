b6c9df62daab5ed082e34073773848964e2d295c
Aluno <Aluno@DECOM-LAB-074>
2022-10-24 07:43:32 -0300

    Criação e implementação do BD desktop #RF006 #6

--- Farmacia/src/main/java/com/mycompany/farmacia/Farmacia.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/Farmacia.java
@@ -1,13 +1,16 @@
 package com.mycompany.farmacia;
 
-import com.mycompany.farmacia.view.TelaInicial;
+import com.mycompany.farmacia.dao.ProdutoDAO;
+import com.mycompany.farmacia.dto.Rotulo;
 
 public class Farmacia {
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaInicial().setVisible(true);
+                
+                ProdutoDAO.cadastrarProduto(new Rotulo(1, "Teste"), 2, 1.00, false, "Dorflex", "10/10/2023");
+                //new TelaInicial().setVisible(true);
             }
         });
     }
--- Farmacia/src/main/java/com/mycompany/farmacia/bancoDeDados/EstoqueBD.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/bancoDeDados/EstoqueBD.java
@@ -1,29 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.farmacia.bancoDeDados;
 
 import java.sql.*;
 
-/**
- *
- * @author Aluno
- */
 public class EstoqueBD {
-
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://farmaciagrupo4:3306/estoque";
-    private static final String USER = "drogaspoint";
-    private static final String PASS = "cefet123";
-    //private static Connection con = null;
-
-    /*public static void main(String[] args) {
-        EstoqueBD conexao = new EstoqueBD();
-        Connection conn = conexao.conectar();
-        //conexao.desconectar(conn);
-    }*/
 
     public static Connection conectar() {
         Connection conn = null;
@@ -39,22 +18,16 @@ public class EstoqueBD {
 
         return conn;
     }
-    
-    public void desconectar(Connection conn) {
+
+    public static void desconectar(Connection conn) {
         try {
-          if(conn != null && !conn.isClosed()) {
-            conn.close();
-            System.out.println("Desconectou do banco de dados.");
-          }
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+                System.out.println("Desconectou do banco de dados.");
+            }
         } catch (SQLException ex) {
-          System.out.println("Não conseguiu desconectar do BD.");
+            System.out.println("Não conseguiu desconectar do BD.");
         }
-  }
-    
-    
-    public static void main(String[] args) {
-        EstoqueBD.conectar();
     }
-    /**/
 
 }
