dea5ca175deac2cb12ea330b0134cb7de554dee6
occlude <aaribeiro10@gmail.com>
2022-10-31 08:10:04 -0300

    "Implementação da interface desktop #RF005 #5"

--- Farmacia/src/main/java/com/mycompany/farmacia/view/TelaEstoque.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/view/TelaEstoque.java
@@ -75,13 +75,13 @@ public class TelaEstoque extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Código", "Quantidade", "Nome", "Receita", "Valor"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
--- Farmacia/src/main/java/com/mycompany/farmacia/view/TelaRelatorio.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/view/TelaRelatorio.java
@@ -58,13 +58,13 @@ public class TelaRelatorio extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Data", "Código", "Quantidade", "Nome", "Valor"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
