74116db3cda625f94e8153a73bd0f9cc081b924e
iszxsz <isafflorencio@gmail.com>
2022-10-31 09:39:13 -0300

    Criação e implementação do Login #RF007 #7

--- Farmacia/src/main/java/com/mycompany/farmacia/dao/LoginDAO.java
+++ Farmacia/src/main/java/com/mycompany/farmacia/dao/LoginDAO.java
@@ -5,19 +5,45 @@
 package com.mycompany.farmacia.dao;
 
 import com.mycompany.farmacia.bancoDeDados.EstoqueBD;
+import com.mycompany.farmacia.dto.Produto;
+import com.mycompany.farmacia.view.TelaInicial;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author aluno
  */
 public class LoginDAO {
-    public static void verificarLogin(String usuario, String senha){
+    public static void verificarLogin(String nome, String senha){
         Connection conn = EstoqueBD.conectar();
         
         try{
-            String consultar = "SELECT * FROM `login` WHERE usuario = '" + usuario + "'";
+            String consultar = "SELECT * FROM `estoque` WHERE nome = '" + nome + "'";
             ResultSet r = null;
+            
+            Statement stm = conn.createStatement();
+            r = stm.executeQuery(consultar);
+            while(r.next()){
+                if(nome.equals(r.getString("usuario"))){
+                    if(senha.equals(r.getString("senha"))){
+                        java.awt.EventQueue.invokeLater(new Runnable() {
+                            public void run() {
+                            new TelaInicial().setVisible(true);
+                        }
+                        });
+                    }
+                    
+            }}
+            
+            r.close();
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu consultar um produto no BD.");
+        } finally {
+           EstoqueBD.desconectar(conn);
+        }
+    }
     }
 }
