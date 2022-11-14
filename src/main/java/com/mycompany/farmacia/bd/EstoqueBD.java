0c82c07b90c64ecbba7b8f50257f420f5dbee987
Aluno <Aluno@DECOM-LAB-074>
2022-11-14 08:01:01 -0300

    Criação e implementação do BD desktop #RF006

--- src/main/java/com/mycompany/farmacia/bd/EstoqueBD.java
+++ src/main/java/com/mycompany/farmacia/bd/EstoqueBD.java
@@ -4,11 +4,11 @@ import java.sql.*;
 
 public class EstoqueBD {
 
-    /*public static Connection conectar() {
+    public static Connection conectar() {
         Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://db4free.net:3306/farmaciagrupo4", "drogaspoint", "cefet123");
+            conn = DriverManager.getConnection("jdbc:mysql://drogaspoint.cbl5egq4cigg.us-east-1.rds.amazonaws.com:3306/drogaspoint", "admin", "cefet123");
             System.out.println("Conectou no banco de dados.");
         } catch (SQLException ex) {
             System.out.println("Erro: Não conseguiu conectar no BD.");
@@ -28,9 +28,9 @@ public class EstoqueBD {
         } catch (SQLException ex) {
             System.out.println("Não conseguiu desconectar do BD.");
         }
-    }*/
+    }
     
-    public static Connection conectar() {
+    /*public static Connection conectar() {
         Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -54,6 +54,6 @@ public class EstoqueBD {
         } catch (SQLException ex) {
             System.out.println("Não conseguiu desconectar do BD.");
         }
-    }
+    }*/
 
 }
