d3669519261d9a4d3d507a46157b8cc8db1d5e01
iszxsz <isafflorencio@gmail.com>
2022-11-30 09:22:56 -0200

    correção de erros #10

---
+++ farmacia-model/src/main/java/com/mycompany/farmacia/dao/ProdutoDAO.java
@@ -0,0 +1,148 @@
+package com.mycompany.farmacia.dao;
+
+import com.mycompany.farmacia.bd.EstoqueBD;
+import com.mycompany.farmacia.dto.Produto;
+import com.mycompany.farmacia.dto.Rotulo;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDAO {
+    private static List<Produto> produtos;
+    
+    static{
+        produtos = new ArrayList<>();
+    }
+    
+    private ProdutoDAO(){}
+    
+    public static void consultarBD(){
+        Connection conn = EstoqueBD.conectar();
+        produtos.clear();
+        
+        try{
+            String consultar = "SELECT * FROM `estoque`";
+            ResultSet r = null;
+            
+            Statement stm = conn.createStatement();
+            r = stm.executeQuery(consultar);
+            while(r.next()){
+                produtos.add(new Produto(RotuloDAO.consultarRotuloPorCodigo(r.getInt("rotulo")), r.getInt("codigo"), r.getDouble("valor"), r.getBoolean("receita"), r.getString("nome"), r.getString("validade")));
+            }
+            
+            r.close();
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu consultar um produto no BD.");
+        } finally {
+           EstoqueBD.desconectar(conn);
+        }
+        
+    }
+    
+    public static void consultarPorNome(String nome){
+        Connection conn = EstoqueBD.conectar();
+        produtos.clear();
+        
+        try{
+            String consultar = "SELECT * FROM `estoque` WHERE nome = '" + nome + "'";
+            ResultSet r = null;
+            
+            Statement stm = conn.createStatement();
+            r = stm.executeQuery(consultar);
+            while(r.next()){
+                produtos.add(new Produto(RotuloDAO.consultarRotuloPorCodigo(r.getInt("rotulo")), r.getInt("codigo"), r.getDouble("valor"), r.getBoolean("receita"), r.getString("nome"), r.getString("validade")));
+            }
+            
+            r.close();
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu consultar um produto no BD.");
+        } finally {
+           EstoqueBD.desconectar(conn);
+        }
+        
+    }
+    
+    public static void consultarPorCodigo(int codigo){
+        Connection conn = EstoqueBD.conectar();
+        produtos.clear();
+        
+        try{
+            String consultar = "SELECT * FROM `estoque` WHERE codigo = '" + codigo + "'";
+            ResultSet r = null;
+            
+            Statement stm = conn.createStatement();
+            r = stm.executeQuery(consultar);
+            while(r.next()){
+                produtos.add(new Produto(RotuloDAO.consultarRotuloPorCodigo(r.getInt("rotulo")), r.getInt("codigo"), r.getDouble("valor"), r.getBoolean("receita"), r.getString("nome"), r.getString("validade")));
+            }
+            r.close();
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu consultar um produto no BD.");
+        } finally {
+           EstoqueBD.desconectar(conn);
+        }
+        
+    }
+    
+    public static void cadastrarProdutoEstoque(Rotulo rotulo, int codigo, double valor, boolean receita, String nome, String validade){
+        Produto p;
+        p = new Produto(rotulo, codigo, valor, receita, nome, validade);
+        Connection conn = EstoqueBD.conectar();
+        try {
+            String adicionar = "INSERT INTO `estoque` (rotulo, codigo, valor, receita, nome, validade) VALUES ('" + rotulo.getCodigo() + "', " + p.getCodigo() + ", " + p.getValor() +", " + p.getReceita() +", '" + p.getNome() +"', '" + p.getValidade() + "')";
+
+            Statement stm = conn.createStatement();
+            stm.execute(adicionar);
+            System.out.println("Adicionou no BD.");
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu adicionar um produto no BD.");
+        } finally {
+           EstoqueBD.desconectar(conn);
+        }
+    }
+    
+    public static void removerProdutoEstoque(Produto p){
+        Connection conn = EstoqueBD.conectar();
+        try {
+            String adicionar = "DELETE FROM estoque WHERE codigo = " + p.getCodigo();
+
+            Statement stm = conn.createStatement();
+            stm.execute(adicionar);
+            System.out.println("Removeu do BD.");
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu remover um produto no BD.");
+        } finally {
+           EstoqueBD.desconectar(conn);
+        }
+    }
+    
+    public static int aderirCodigo(){
+        int maior = 0;
+        Connection conn = EstoqueBD.conectar();
+        try{
+            String consultar = "SELECT * FROM `estoque`";
+            ResultSet r = null;
+            
+            Statement stm = conn.createStatement();
+            r = stm.executeQuery(consultar);
+            while(r.next()){
+                if(r.getInt("codigo") > maior)
+                    maior = r.getInt("codigo");
+            }
+            r.close();
+        } catch (SQLException ex) {
+            System.out.println("Não conseguiu consultar um produto no BD.");
+        } finally {
+           EstoqueBD.desconectar(conn);
+           return maior + 1;
+        }
+    }
+    
+    public static List<Produto> listarProdutos(){    
+        return produtos;
+        //lista
+    }
+}
---
+++ farmacia-model/src/main/java/com/mycompany/farmacia/servico/ManterProduto.java
@@ -0,0 +1,35 @@
+package com.mycompany.farmacia.servico;
+
+import com.mycompany.farmacia.common.NegocioException;
+import com.mycompany.farmacia.dao.ProdutoDAO;
+import com.mycompany.farmacia.dao.RotuloDAO;
+import com.mycompany.farmacia.dto.Produto;
+import java.util.List;
+
+public class ManterProduto {
+    public static void adicionarProduto(String nomeRotulo, double valor, boolean receita, String nome, String validade) throws NegocioException{
+        if((nomeRotulo.isEmpty()) || (valor == 0) || (validade.isEmpty()) || (nome.isEmpty())){
+            throw new NegocioException("Preencha todos os campos!");
+        } else{
+            ProdutoDAO.cadastrarProdutoEstoque(RotuloDAO.aderirRotulo(nomeRotulo), ProdutoDAO.aderirCodigo(), valor, receita, nome, validade);
+        }
+    }
+    
+    public static List<Produto> consultarProduto(String info) throws NegocioException{
+        if (info.isEmpty()) {
+            throw new NegocioException("Preencha o campo de pesquisa!");
+        } else {
+            if (Generics.isInteger(info)) {
+                ProdutoDAO.consultarPorCodigo(Integer.parseInt(info));
+            } else {
+                ProdutoDAO.consultarPorNome(info);
+            }
+        }
+        return ProdutoDAO.listarProdutos();
+    }
+    
+    public static List<Produto> listarProduto(){
+        ProdutoDAO.consultarBD();
+        return ProdutoDAO.listarProdutos();
+    }
+}
